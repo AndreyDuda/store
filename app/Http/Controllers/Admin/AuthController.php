10189650c5a6515604bc1c396a1d90726c30b635
Andrey Duda <andreyduda88@gmail.com>
2018-04-14 21:01:30 +0300

    Excel

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
 
     public function index()
     {
-        return 'Admin';
+       return view('home');
     }
 
     /**
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Http\Response
+
      */
     public function index()
     {
