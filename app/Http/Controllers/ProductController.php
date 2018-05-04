50ef226331d521ae433c39b84365270b25cbfb23
Andrey Duda <andreyduda88@gmail.com>
2018-05-04 20:02:41 +0300

    create controllers for store

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+
+    public function product()
+    {
+
+    }
+
+}
