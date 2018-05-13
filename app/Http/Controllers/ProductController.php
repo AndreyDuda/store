3b55edfb4f6c1d4fc677b985f345c71247e19bdc
Andrey Duda <andreyduda88@gmail.com>
2018-05-13 15:23:42 +0300

    add view ProductController (product and products)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,18 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\ProductRepository;
 
-class ProductController extends Controller
+class ProductController extends SiteController
 {
-    //
+    public function __construct(ProductRepository $product_rep)
+    {
+        $this->product_rep = $product_rep;
+
+        $this->template = env('THEME') . '.index';
+    }
+
     public function index()
     {
-        return 'Все товары + параметры фильтров';
+        $products = $this->product_rep->getAll();
+        $content = view(env('THEME') . '.product.products')->render();
+        $this->vars = array_add($this->vars, 'content', $content);
+        return $this->renderOutput();
     }
 
     public function product()
     {
-        return 'Один товар с описанием';
+        $products = $this->product_rep->getOne('1');
+        $content = view(env('THEME') . '.product.product')->render();
+        $this->vars = array_add($this->vars, 'content', $content);
+        return $this->renderOutput();
     }
 
 }
