fe7bfabdcd12e79ce4fb4d465997a9749d1e15ed
Andrey Duda <andreyduda88@gmail.com>
2018-06-06 20:13:53 +0300

    view admin

---
+++ resources/views/storejeans/admin/admin.php
@@ -0,0 +1,811 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Story Jeans</title>
+	<link rel="icon" href="img/favicon.ico">
+	<link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="libs/slick/css/slick.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Gentium+Basic" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/media.css">
+</head>
+<body>
+	<header>
+		<div class="header-desktop" >
+			<div class="logo">
+
+				<a href="index.php">
+					<img src="img/logo.png" alt="" >
+				</a>
+			</div>
+			<div class="menu-left">
+				<a href="index.php">На сайт</a>
+				<a href="№">Регистрация</a>
+				<a href="#">Заказы</a>
+				<a href="#">Настройки профиля</a>
+			</div>
+			<div class="input-wrapper search" data-text="">
+				<input type="text" placeholder="Поиск  по сайту…">          
+			</div>
+			<!-- <div class="menu-right"> -->
+
+				<div class="contacts-wrap">
+					<div class="phones phones-header">
+						<p class="icon mtc"> 099 378 33 31</p>
+						<p class="icon kievstar"> 096 002 65 69</p>
+					</div>
+				</div>
+			</div>
+			<div class="header-mobile">
+				<div class="menu-up">
+					<a href="index.php" class="logo-mobile">
+						<img src="img/1_whitemini_logo.png" alt="">
+					</a>
+					<a href="tel:0993783331" class="phone ">
+						099 378 33 31
+					</a>
+					<a href="cart.php" class="cart">
+						<i class="fa fa-shopping-cart " aria-hidden="true"></i>
+						<span>0</span>
+					</a>
+					<div class="burger">
+						<input type="checkbox" id="burger_check" class="burger_checkbox">
+						<label for="burger_check">
+							<i class="fa fa-bars"></i>
+						</label>
+					</div>
+				</div>
+				<div class="menu-left-mobile">
+					<a href="index.php">На сайт</a>
+					<a href="№">Регистрация</a>
+					<a href="#">Заказы</a>
+					<a href="#">Настройки профиля</a>
+				</div>
+			</div>
+		</header>
+		<aside class="aside-2">
+			<dl class=" filters-admin">
+				<dt class="filter-header">
+					Товары 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Дрочнуть</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Обосрать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Категории 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Насцыкать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Настройки 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Замутить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Заказы 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Товары 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Дрочнуть</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Обосрать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Категории 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Насцыкать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Настройки 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Замутить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Заказы 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Товары 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Дрочнуть</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Обосрать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Категории 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Насцыкать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Товары 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Дрочнуть</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Обосрать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Категории 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Насцыкать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Настройки 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Замутить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Настройки 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Замутить</a>
+						</li>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Профиль 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Замучать профиль</a>
+						</li>
+						<li>
+							<a href="#">Удалить печень</a>
+						</li>
+						<li>
+							<a href="#">Редактировать ситуацию в стране</a>
+						</li>
+						<li>
+							<a href="#">Залупить</a>
+						</li>
+					</ul>
+				</dd>
+				<dt class="filter-header">
+					Профиль 
+				</dt>
+				<dd class="filter-body ">
+					<ul>
+						<li>
+							<a href="#">Редактировать</a>
+						</li>
+						<li>
+							<a href="#">Удалить</a>
+						</li>
+						<li>
+							<a href="#">Замучать профиль</a>
+						</li>
+						<li>
+							<a href="#">Удалить печень</a>
+						</li>
+						<li>
+							<a href="#">Редактировать ситуацию в стране</a>
+						</li>
+						<li>
+							<a href="#">Залупить</a>
+						</li>
+					</ul>
+				</dd>
+
+			</dl>
+		</aside>
+		<div class="content">
+			<h2 class="table-name">Table name</h2>
+			<table class="admin-table">
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+				<tr>
+					<td>
+						id товара
+					</td>
+					<td>
+						title
+					</td>
+					<td>
+						decription
+					</td>
+					<td>
+						foto
+					</td>
+					<td>
+						another
+					</td>
+				</tr>
+			</table>
+			<a href="#">показать ещё 50 товаров >></a>
+			<span class="table-text">Общее количество - 1546 товаров</span>
+			<span class="table-text">Товаров на сумму - 23456789 грн. </span>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.0/jquery.min.js"></script><!-- JQuery -->
+<script src="js/common.js"></script><!-- My JS file -->
